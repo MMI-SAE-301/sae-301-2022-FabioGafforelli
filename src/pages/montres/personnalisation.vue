4bc9f006487d82f5f452b3fd863b6dea0676c2d5
FabioGafforelli <fabio.gafforelli@edu.univ-fcomte.fr>
2022-10-25 16:46:29 +0200

    Début création page personnalisation

---
+++ src/pages/montres/personnalisation.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import MontreCarree from '@/components/MontreCarree.vue';
+
+
+</script>
+<template>
+  <div class="grid grid-flow-row-dense grid-cols-[repeat(auto-fit,minmax(350px,1fr))] gap-5  lg:mx-10">
+    <h1 class="">Bienvenue dans notre expérience de Personnalisation</h1>
+  </div>
+  <div class="grid grid-flow-row-dense grid-cols-3 grid-rows-3">
+  <div class="col-span-2 w-64">
+    <MontreCarree />
+  </div>
+  <div class="col-span-2 text-bleuroi">Armand Guy</div>
+</div>
+</template>
