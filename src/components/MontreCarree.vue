168e21a89b10889653135b83516c77fd7be3711d
FabioGafforelli <fabio.gafforelli@edu.univ-fcomte.fr>
2022-10-25 09:21:19 +0200

    import du type ts montre

---
+++ src/components/MontreCarree.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+    import type { Montre} from "@/types"
+
+withDefaults(defineProps<Montre>(),{
+    bracelet: "white",
+    empeigne: "white",
+    pointe: "white",
+})
+</script>
+
+<template>
+    
+</template>
