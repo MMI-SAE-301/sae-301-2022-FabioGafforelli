b5df7846df405a6bcd3adca0b3521288f9eacc1b
FabioGafforelli <fabio.gafforelli@edu.univ-fcomte.fr>
2022-10-25 11:01:38 +0200

    création de la page edit

---
+++ src/pages/montres/edit/[id].vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import FormMontreCarree from '@/components/FormMontreCarree.vue';
+
+defineProps<{
+  id: string;
+}>();
+</script>
+<template>
+  <FormMontreCarree :id="id" />
+</template>
