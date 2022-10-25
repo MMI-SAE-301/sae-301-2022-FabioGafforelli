9984f99f6906e45414e2fd83356c9105aa5f3058
FabioGafforelli <fabio.gafforelli@edu.univ-fcomte.fr>
2022-10-25 15:03:16 +0200

    Login google mise en forme

--- src/components/LoginGoogle.vue
+++ src/components/LoginGoogle.vue
@@ -2,12 +2,12 @@
 import { supabase, user } from '../supabase';
 </script>
 <template>
-<div>
+<div class="border bg-red-600">
     <button v-if="user" @pointerdown="supabase.auth.signOut()">
         Se déconnecter ({{user.email}})
     </button>
-    <button v-else @pointerdown="supabase.auth.signIn({provider: 'google'})">
-        Se connecter avec Facebook
+    <button class="text-white" v-else @pointerdown="supabase.auth.signIn({provider: 'google'})">
+        Se connecter avec Google
     </button>
 </div>
 </template> 
