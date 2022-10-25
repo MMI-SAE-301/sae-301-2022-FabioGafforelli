15d84a24552e5835e855b6e4646f601ccd0bf064
FabioGafforelli <fabio.gafforelli@edu.univ-fcomte.fr>
2022-10-25 09:12:13 +0200

    création index montre

---
+++ src/pages/montres/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import ListeMontres from "../../components/ListeMontres.vue";
+
+</script>
+<template>
+  <div class="grid grid-flow-row-dense grid-cols-[repeat(auto-fit,minmax(350px,1fr))] gap-5  lg:mx-10">
+    <h2>Liste des montres perso de l'utlisateur</h2>
+    <ListeBaskets />
+  </div>
+</template>
