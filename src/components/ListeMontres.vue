54f6e7212034ad95278a4d19666d4ce7f8ae9a26
FabioGafforelli <fabio.gafforelli@edu.univ-fcomte.fr>
2022-10-25 09:12:53 +0200

    ListeMontres

---
+++ src/components/ListeMontres.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { supabase } from "@/supabase";
+import MontreCarre from "./MontreCarre.vue";
+const props = defineProps<{
+  max?: number;
+}>();
+
+let { data: Montres, error } = await supabase
+  .from('montre')
+  .select('*')
+
+  .limit(props.max ?? 100)
+if (error) {
+  console.log("n'a pas pu récupérer les baskets", { error });
+}
+</script>
+<template>
+  <ul>
+    <li v-for="montre in Montres" :key="montre.id_montre">
+      <router-link
+        :to="{ name: 'basket-edit-id', params: { id: montre.id } }"
+      >
+        <MontreCarre class="w-64" v-bind="montre" />
+      </router-link>
+    </li>
+  </ul>
+</template>
--- src/pages/montres/index.vue
+++ src/pages/montres/index.vue
@@ -5,6 +5,6 @@ import ListeMontres from "../../components/ListeMontres.vue";
 <template>
   <div class="grid grid-flow-row-dense grid-cols-[repeat(auto-fit,minmax(350px,1fr))] gap-5  lg:mx-10">
     <h2>Liste des montres perso de l'utlisateur</h2>
-    <ListeBaskets />
+    <ListeMontres />
   </div>
 </template>
