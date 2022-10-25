aff140761b2d6746e1108c65a1688661a8c54f19
FabioGafforelli <fabio.gafforelli@edu.univ-fcomte.fr>
2022-10-25 13:34:14 +0200

    Liste montres

--- src/pages/montres/Liste.vue
+++ src/pages/montres/Liste.vue
@@ -3,13 +3,17 @@ import MontreCarre from "@/components/MontreCarree.vue";
 import type { Montre } from "@/types";
 import { supabase, user } from '@/supabase';
 import ListeMontres from '@/components/ListeMontres.vue';
-import Login from "@/components/Login.vue";
 
 </script>
 <template>
- <section>
-  <h1>Connectez vous afin d'accèder à la liste de vos montres personnalisées</h1>
- </section>
- <Login />
-
+  <section v-if="user">
+  <h2>
+    un extrait de vos montres (<RouterLink
+      class="text-red-600 underline"
+      to="/montres"
+      >Toutes les voir</RouterLink
+    >)
+  </h2>
+  <ListeMontres class="flex flex-wrap gap-2" :max="100" />
+</section>
 </template>
