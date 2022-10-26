55f1a8c81d78f0cc175a1835e809fd652332fd67
FabioGafforelli <fabio.gafforelli@edu.univ-fcomte.fr>
2022-10-26 11:34:20 +0200

    Import du logo google en svg

--- src/components/LoginGoogle.vue
+++ src/components/LoginGoogle.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import { supabase, user } from '../supabase';
+import LogoGoogle from './LogoGoogle.vue';
 </script>
 <template>
 <div class="flex justify-center  ">
                 <div class=" mt-auto p-16 border-neutral-50 border-2 ">
     <button class="text-white" @pointerdown="supabase.auth.signIn({provider: 'google'})">
+        <LogoGoogle />
         Se connecter avec Google
     </button>
 </div>
