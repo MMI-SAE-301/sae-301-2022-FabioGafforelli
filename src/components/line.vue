fa012a29edd132c9182a62d929b85a7b9d2fe837
FabioGafforelli <fabio.gafforelli@edu.univ-fcomte.fr>
2022-10-26 10:32:52 +0200

    création du composant line.vue

---
+++ src/components/line.vue
@@ -0,0 +1,32 @@
+<template>
+<svg
+  width="126"
+  height="7"
+  viewBox="0 0 126 7"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+  preserveAspectRatio="xMidYMid meet"
+>
+  <line
+    x1="0.666504"
+    y1="3.5"
+    x2="125.111"
+    y2="3.5"
+    stroke="url(#paint0_linear_1_179)"
+    stroke-width="7"
+  ></line>
+  <defs>
+    <linearGradient
+      id="paint0_linear_1_179"
+      x1="125.111"
+      y1="7.51909"
+      x2="15.7523"
+      y2="7.51911"
+      gradientUnits="userSpaceOnUse"
+    >
+      <stop offset="0.0135415" stop-color="#0C1673"></stop>
+      <stop offset="1" stop-color="#040938"></stop>
+    </linearGradient>
+  </defs>
+</svg>
+</template>
