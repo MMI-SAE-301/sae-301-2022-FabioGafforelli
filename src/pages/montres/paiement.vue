25b1fe00f095cd907756347bf7e2f5aa05ae683f
FabioGafforelli <fabio.gafforelli@edu.univ-fcomte.fr>
2022-10-27 00:02:05 +0200

    Ajout de la carte bancaire dans le formulaire

--- src/pages/montres/paiement.vue
+++ src/pages/montres/paiement.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import MontreCarree from '@/components/MontreCarree.vue';
+import Carte from '@/components/icones/carte.vue';
 </script>
 
 <template>
@@ -13,6 +14,7 @@ import MontreCarree from '@/components/MontreCarree.vue';
                 Paiement par carte :
             </h3>
 <br>
+<Carte />
 <FormKit type="form">
     <div class="grid grid-cols-2">
     <FormKit
