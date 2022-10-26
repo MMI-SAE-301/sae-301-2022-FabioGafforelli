9026e43b5060bb77fccf6e0c3c5d776a20853aa2
FabioGafforelli <fabio.gafforelli@edu.univ-fcomte.fr>
2022-10-26 23:16:34 +0200

    Import de la montre dans paiement

--- src/pages/montres/paiement.vue
+++ src/pages/montres/paiement.vue
@@ -1,7 +1,13 @@
 <script setup lang="ts">
-
+import MontreCarree from '@/components/MontreCarree.vue';
 </script>
 
 <template>
-    
+     <main clas="bg-red-700 grid grid-flow-row-dense grid-cols-[repeat(auto-fit,minmax(350px,1fr))] gap-5  lg:mx-10">
+    <div class="grid grid-cols-2">
+        <div class="">
+            <MontreCarree class=" w-64" v-bind="montre" id="profil" />
+        </div>
+        </div>
+        </main>
 </template>
