367aca67b202887043f94c05bc16b8e175b1174e
FabioGafforelli <fabio.gafforelli@edu.univ-fcomte.fr>
2022-10-26 11:25:45 +0200

    css login facebook

--- src/components/LoginFacebook.vue
+++ src/components/LoginFacebook.vue
@@ -2,9 +2,11 @@
 import { supabase, user } from '../supabase';
 </script>
 <template>
-<div>
+<div class="flex justify-center  ">
+                <div class=" mt-auto p-16 border-neutral-50 border-2 ">
     <button @pointerdown="supabase.auth.signIn({provider: 'facebook'})">
         Se connecter avec Facebook
     </button>
+</div>
 </div>
 </template> 
