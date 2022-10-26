ac426d16948fa58f89430fab933635c7ac57b6f4
FabioGafforelli <fabio.gafforelli@edu.univ-fcomte.fr>
2022-10-27 00:03:58 +0200

    Page permettant de choisir son mode de paiement

---
+++ src/pages/montres/paiement.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import MontreCarree from '@/components/MontreCarree.vue';
+import Carte from '@/components/icones/carte.vue';
+</script>
+
+<template>
+     <main clas="bg-red-700 grid grid-flow-row-dense grid-cols-[repeat(auto-fit,minmax(350px,1fr))] gap-5  lg:mx-10">
+    <div class="grid grid-cols-2">
+        <div class="">
+            <MontreCarree class=" w-64"  id="profil" />
+        </div>
+        <div>
+            <h3 class="text-xl font-bold">
+                Choix du mode de paiement :
+            </h3>
+            
+<br>
+</div>
+</div>
+        </main>
+</template>
