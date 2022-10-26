6c98e92bcc7cfa07d80078b502d49f7356032e00
FabioGafforelli <fabio.gafforelli@edu.univ-fcomte.fr>
2022-10-26 20:20:23 +0200

    Début refonte headeer mobile

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,41 +1,70 @@
 <script setup lang="ts">
-import Logo from "./Logo.vue";
-import { supabase, user } from "@/supabase";
+import Logo from './Logo.vue'
+import { Bars3Icon } from "@heroicons/vue/20/solid";
 
 </script>
 
 <template>
-    <header class=" bg-bleuroi drop-shadow-lg z-10 top-0 left-0 right-0 px-5 lg:py-8 py-3">
-        <a href="#content" class="sr-only focus:not-sr-only text-lg text-blanc"> Passez au contenu </a>
-        <nav class=" lg:flex lg:justify-between items-center">
-          <div class="flex justify-between items-center">
-           <RouterLink to="/">		
-           <Logo class="w-24" />
-           <h1 class="text-blanc-50 text-xl font-bold lg:text-3xl text-blanc">Tik - Tak</h1>
-        </RouterLink> 	
-          <span class="text-3xl cursor-pointer lg:hidden block w-8 h-8 mr-25  ">
-            <span class="sr-only ">Menu</span>
-          </span>
-          </div>
-          <ul class="lg:flex  gap-6 lg:items-center text-blanc text-s font-algerian mx-6 py-4 hidden" >
-            <li class="my-3">
-              <RouterLink class="my-6 lg:px-4 xl:pt-2 xl:rounded-sm font-museomoderno text-blanc  " to="/montres/personnalisation">Personnalisation</RouterLink>
-            </li>
-            <li class="my-3" v-if="user">
-              <RouterLink class="my-6 lg:px-4 xl:pt-2 xl:rounded-sm font-museomoderno text-blanc  " to="/montres">Mes montres</RouterLink>
-            </li>
-            <li class="my-3" v-else>
-              <RouterLink class="my-6 lg:px-4 xl:pt-2 xl:rounded-sm font-museomoderno text-blanc  " to="/montres/inspiration">Inspirations</RouterLink>
-            </li>
-            <li class="my-3">
-              <RouterLink class="my-6 lg:px-4 xl:pt-2 xl:rounded-sm font-museomoderno text-blanc " to="/reglement"></RouterLink>
-            </li>
-            <li class="my-3">
-              <RouterLink class="my-6 lg:px-4 xl:pt-2 xl:rounded-sm font-museomoderno text-blanc " to="/login">Se Connecter</RouterLink>
-            </li>
-          </ul>    
-        </nav>
-      </header>
-    </template>
-  
-    
+<header class=" bg-bleuroi top-0 drop-shadow-lg z-10 ">
+    <a href="#content" class="sr-only focus:not-sr-only text-lg text-white"> Passez au contenu </a>
+    <nav class=" lg:flex lg:justify-between items-center">
+      <div class="flex justify-between items-center">
+       <RouterLink to="/">
+        <Logo class="w-36 h-24" />
+           <h1 class="text-blanc text-xl font-bold lg:text-3xl">Tik - Tak</h1>
+       </RouterLink>
+      <span class="text-3xl cursor-pointer lg:hidden block w-8 h-8 mx-4  ">
+        <Bars3Icon class="text-blanc"  aria-controls="menu"
+        :aria-expanded="menuOuvert"
+        @click="menuOuvert = !menuOuvert">
+        </Bars3Icon>
+        <span class="sr-only ">Menu</span>
+      </span>
+      </div>
+      <Transition
+        enter="transition duration-2000 ease-out"
+        enterFrom="transform translate-x-full opacity-0"
+        enterTo="transform translate-x-0 opacity-100"
+        leave="transition duration-1750 ease-out"
+        leaveFrom="transform  translate-x-0 opacity-100"
+        leaveTo="transform translate-x-full opacity-0"
+      >
+      <ul id="menu" v-if="menuOuvert" class="lg:hidden bg-white w-full text- text-xl font-timmana  py-4 text-center" >
+        <li class="my-3">
+          <RouterLink class="my-6 lg:px-4 xl:pt-2 xl:rounded-sm font-algerian text-black  " to="/programmation">Programmation</RouterLink>
+        </li>
+        <li class="my-3">
+          <RouterLink class="my-6 lg:px-4 xl:pt-2 xl:rounded-sm font-algerian text-black " to="/artistes">Artistes</RouterLink>
+        </li>
+        <li class="my-3">
+          <RouterLink class="my-6 lg:px-4 xl:pt-2 xl:rounded-sm text-black font-algerian " to="/festival">Festival</RouterLink>
+        </li>
+        <li class="my-3">
+          <RouterLink class="my-6 lg:px-4 xl:pt-2 xl:rounded-sm font-algerian text-black " to="/billetterie">Billetterie</RouterLink>
+        </li>
+        <li class="my-3">
+          <RouterLink class="my-6 lg:px-4 xl:pt-2 xl:rounded-sm font-algerian text-black" to="/contact">Contact</RouterLink>
+        </li>
+      </ul>
+      </Transition>
+      <ul class="lg:flex  gap-6 lg:items-center text-white text-xl font-algerian mx-6 py-4 hidden" >
+        <li class="my-3">
+          <RouterLink class="my-6 lg:px-2 xl:pt-2 xl:rounded-sm font-algerian text-white" to="/programmation">Programmation</RouterLink>
+        </li>
+        <li class="my-3">
+          <RouterLink class="my-6 lg:px-2 xl:pt-2 xl:rounded-sm font-algerian text-white " to="artistes">Artistes</RouterLink>
+        </li>
+        <li class="my-3">
+          <RouterLink class="my-6 lg:px-2 xl:pt-2 xl:rounded-sm font-algerian text-white " to="/festival">Festival</RouterLink>
+        </li>
+        <li class="my-3">
+          <RouterLink class="my-6 lg:px-2 xl:pt-2 xl:rounded-sm font-algerian text-white " to="/billetterie">Billetterie</RouterLink>
+        </li>
+        <li class="my-3">
+          <RouterLink class="my-6 lg:px-2 xl:pt-2 xl:rounded-sm font-algerian text-white " to="/contact">Contact</RouterLink>
+        </li>
+      </ul>    
+    </nav>
+  </header>
+
+</template>
