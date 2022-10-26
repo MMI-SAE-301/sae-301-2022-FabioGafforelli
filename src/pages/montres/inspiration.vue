cbd32e23f1aa6cbb1c1d0d3253701b334b3eb895
FabioGafforelli <fabio.gafforelli@edu.univ-fcomte.fr>
2022-10-26 17:15:52 +0200

    Page inspiration

---
+++ src/pages/montres/inspiration.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import MontreCarre from "@/components/MontreCarree.vue";
+import type { Montre } from "@/types";
+import { supabase, user } from '@/supabase';
+import ListeMontres from '@/components/ListeMontres.vue';
+
+</script>
+<template>
+  <section v-if="user">
+  <h2>
+    un extrait de vos montres (<RouterLink
+      class="text-red-600 underline"
+      to="/montres"
+      >Toutes les voir</RouterLink
+    >)
+  </h2>
+  <ListeMontres class="flex flex-wrap gap-2" :max="100" />
+</section>
+</template>
