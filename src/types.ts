4a79ea9d08550ed459c3e9a6873c818a3ca37686
FabioGafforelli <fabio.gafforelli@edu.univ-fcomte.fr>
2022-10-26 15:01:33 +0200

    Réduction du nombre d'options dans le formulaire

--- src/types.ts
+++ src/types.ts
@@ -9,11 +9,4 @@ export interface Montre {
     "#FF0000": "Rouge",
     "#00FF00": "Vert",
     "#E6007E": "Rose",
-    "#FFFF00": "Jaune",
-    "#753B5B": "Mauve",
-    "#00FFFF": "Cyan",
-    "#C0C0C0": "Gris clair",
-    "#808080": "Gris",
-    "#800000": "Marron",
-    "#808000": "Olive",
   };
