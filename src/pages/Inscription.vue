993d7ecf53a0266a8748a492bcbbd1617939ef70
FabioGafforelli <fabio.gafforelli@edu.univ-fcomte.fr>
2022-10-26 11:49:20 +0200

    Page Inscription

---
+++ src/pages/Inscription.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import Login from '@/components/Login.vue';
+import LoginFacebook from '@/components/LoginFacebook.vue';
+import LoginGoogle from '@/components/LoginGoogle.vue';
+import header from '@/components/header.vue';
+import { supabase, user } from '@/supabase';
+
+</script>
+
+<template>
+    <h1 class="text-center text-4xl font-bold text-bleuroi">S'INSCRIRE ?</h1>
+    <br>
+    <div class="grid grid-cols-2">
+        <LoginGoogle />
+        <LoginFacebook />
+    </div>
+    <div class="text-center">
+        <Login />
+    </div>
+</template>
