9c3799501d19cc53ae1d96993637966679383e07
FabioGafforelli <fabio.gafforelli@edu.univ-fcomte.fr>
2022-10-26 11:35:49 +0200

    Import du logo de facebook dans le composant login

--- src/components/LoginFacebook.vue
+++ src/components/LoginFacebook.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import { supabase, user } from '../supabase';
+import LogoFacebook from './LogoFacebook.vue';
 </script>
 <template>
 <div class="flex justify-center  ">
                 <div class=" mt-auto p-16 border-neutral-50 border-2 ">
     <button @pointerdown="supabase.auth.signIn({provider: 'facebook'})">
+        <LogoFacebook />
         Se connecter avec Facebook
     </button>
 </div>
