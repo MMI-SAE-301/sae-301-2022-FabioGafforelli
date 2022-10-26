5170ca809e0cf5e9107b4d2fd51bddc896e1c1f6
FabioGafforelli <fabio.gafforelli@edu.univ-fcomte.fr>
2022-10-26 13:02:08 +0200

    Import liste montres perso

--- src/pages/montres/index.vue
+++ src/pages/montres/index.vue
@@ -3,8 +3,11 @@ import ListeMontres from "../../components/ListeMontres.vue";
 
 </script>
 <template>
-  <div class="grid grid-flow-row-dense grid-cols-[repeat(auto-fit,minmax(350px,1fr))] gap-5  lg:mx-10">
+  <h1 class="text-2xl">Mes Montres</h1>
+    <div class="flex flex-wrap gap-4">
+      <div class="flex justify-center">
     <h2>Liste des montres perso de l'utlisateur</h2>
     <ListeMontres />
+    </div>
   </div>
 </template>
