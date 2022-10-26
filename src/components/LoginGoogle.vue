af0e89f0d0a60ddff08a778bcf608ef9103df85e
FabioGafforelli <fabio.gafforelli@edu.univ-fcomte.fr>
2022-10-26 11:25:37 +0200

    css login google

--- src/components/LoginGoogle.vue
+++ src/components/LoginGoogle.vue
@@ -2,9 +2,11 @@
 import { supabase, user } from '../supabase';
 </script>
 <template>
-<div class="border bg-red-600">
+<div class="flex justify-center  ">
+                <div class=" mt-auto p-16 border-neutral-50 border-2 ">
     <button class="text-white" @pointerdown="supabase.auth.signIn({provider: 'google'})">
         Se connecter avec Google
     </button>
+</div>
 </div>
 </template> 
