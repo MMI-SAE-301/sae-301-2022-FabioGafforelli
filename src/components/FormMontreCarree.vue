98f2d0aa66190420c17cda5d428dafde7d1510dd
FabioGafforelli <fabio.gafforelli@edu.univ-fcomte.fr>
2022-10-26 15:42:37 +0200

    import bouton headless ui pour matériaux

--- src/components/FormMontreCarree.vue
+++ src/components/FormMontreCarree.vue
@@ -6,6 +6,12 @@
     import SvgProfil from "./MontreCarree.vue";
 import FormKitListColors from "./FormKitListColors.vue";
 import { Switch } from '@headlessui/vue'
+import {
+      RadioGroup,
+      RadioGroupLabel,
+      RadioGroupDescription,
+      RadioGroupOption,
+    } from '@headlessui/vue'
 
 const enabled = ref(false)
 const router = useRouter();
@@ -15,6 +21,7 @@ const { data: listeMateriaux, error } = await supabase
   .select("*");
 if (error) console.log("n'a pas pu charger la table Matériaux :", error);
 // Les convertir par `map` en un tableau d'objets {value, label} pour FormKit
+
 const optionsMateriaux = listeMateriaux?.map((materiaux) => ({
   value: materiaux.id_materiaux,
   label: materiaux.libelle_materiaux,
@@ -30,14 +37,40 @@ if (props.id) {
     console.log("n'a pas pu charger la table Montre :", error);
   else montre.value = data[0];
 }
-
+const quartierObject: Ref<any> = ref({});
+if (props.id) {
+  // On charge les données de la vue quartiercommune
+  let { data, error } = await supabase
+    .from("montre")
+    .select("*")
+    .eq("id", props.id);
+  if (error || !data)
+    console.log("n'a pas pu charger le table Maison :", error);
+  else quartierObject.value = data[0];
+}
 
 async function upsertMontre (dataForm, node) {
   const { data, error } = await supabase.from("montre").upsert(dataForm);
   if (error) node.setErrors([error.message]);
   else {
     node.setErrors([]);
-    router.push({ name: "montres/listes", params: { id: data[0].id } });
+    router.push({ name: "montres", params: { id: data[0].id } });
+  }
+}
+async function supprimerMontre() {
+  const { data, error } = await supabase
+    .from("montre")
+    .delete()
+    .match({ id: quartierObject.value.id });
+  if (error) {
+    console.error(
+      "Erreur à la suppression de ",
+      quartierObject.value.id,
+      "erreur :",
+      error
+    );
+  } else {
+    router.push("/montres");
   }
 }
 </script>
@@ -48,7 +81,7 @@ async function upsertMontre (dataForm, node) {
         <div class="w-80 bg-bleuroi rounded-2xl pl-10 pr-10 py-10 mb-10">
             <SvgProfil class=" w-64" v-bind="montre" id="profil" />
         </div>
-        <FormKit submit-label="Ajouter au panier" type="form" v-model="montre" @submit="upsertMontre"  >
+        <FormKit :submit-attrs="{ classes: { input: 'bg-bleuroi p-1 rounded2xl text-blanc' } }" submit-label="Ajouter au panier" type="form" v-model="montre" @submit="upsertMontre">
           <div>
                 <h1 class="text-white">Bracelet</h1>
                 <br>
@@ -60,8 +93,35 @@ async function upsertMontre (dataForm, node) {
                 <h1 class="text-white">Matériaux du bracelet</h1>
                 <br>
                 <div class="bg-bleuroi rounded-2xl pl-3 pr-3 py-3 mb-3">
-                  <FormKit type="select" name="id_materiaux"  placeholder="Sélectionner le matériel du bracelet" :options="optionsMateriaux" />             
-            </div>
+                  <RadioGroup v-model="montre.id_materiaux" >
+                    <RadioGroupLabel class="sr-only ">Server size</RadioGroupLabel>
+                        <div class="flex gap-10 ">
+                            <RadioGroupOption class=""
+                                as="template"
+                                v-for="materiaux in listeMateriaux"
+                                :key="materiaux.id_materiaux"
+                                :value="materiaux.id_materiaux"
+                                v-slot="{ active, checked }">
+                                <div :class="[
+                                  active
+                                    ? 'ring-2 ring-noir'
+                                    : '',
+                                  checked ? 'bg-bleuroi text-blanc hover:ring-2 hover:ring-noir ' : ' bg-blanc border-2 hover:ring-2 hover:ring-noir',
+                                    ]"
+                                    class="cursor-pointer rounded-lg px-10 py-4 border-2 border-noir ">
+                                    <div class="text-sm">
+                                        <RadioGroupLabel
+                                            as="p"
+                                            :class="checked ? 'text-blanc' : 'text-noir '"
+                                            class="font-medium ">
+                                            {{ materiaux.libelle_materiaux }}
+                                        </RadioGroupLabel>
+                                    </div>
+                                </div>
+                        </RadioGroupOption>
+                    </div>
+                </RadioGroup>
+                </div>
         </div>
             <br>
             <div>
@@ -79,6 +139,32 @@ async function upsertMontre (dataForm, node) {
                 <FormKitListColors name="ecran" />
             </div>
         </div>
+        <FormKit class="grid grid-cols-2" name="commander" label="Commander" type="checkbox" />
+        <button
+        type="button"
+        v-if="quartierObject.id"
+        @click="($refs.dialogSupprimer as any).showModal()"
+        class="focus-style justify-self-end rounded-md bg-rouge p-2 shadow-sm"
+      >
+        Supprimer l'offre
+      </button>
+      <dialog
+        ref="dialogSupprimer"
+        @click="($event.currentTarget as any).close()"
+      >
+        <button
+          type="button"
+          class="focus-style justify-self-end rounded-md bg-blanc p-2 shadow-sm"
+        >
+          Annuler</button
+        ><button
+          type="button"
+          @click="supprimerMontre()"
+          class="focus-style rounded-md bg-rouge p-2 shadow-sm"
+        >
+          Confirmer suppression
+        </button>
+      </dialog>
         </FormKit>
             <br>
       </div>
