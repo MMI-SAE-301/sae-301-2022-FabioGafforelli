4c6934ca7acd47c7cc306ede08026ecf922d1407
FabioGafforelli <fabio.gafforelli@edu.univ-fcomte.fr>
2022-10-26 11:11:03 +0200

    Import du footer dans l'app

--- src/App.vue
+++ src/App.vue
@@ -33,10 +33,12 @@
   <Suspense>
     <router-view class="m-2 border-2 p-2" />
   </Suspense>
+  <Footer />
 </template>
 
 <script setup lang="ts">
 import { Bars3Icon } from "@heroicons/vue/20/solid";
+import Footer from "./components/footer.vue";
 import Header from "./components/header.vue";
 import Login from "./components/Login.vue";
 </script>
