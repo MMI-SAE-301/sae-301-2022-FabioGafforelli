4fdd4df6120d1276db57d7c0541c132176bcd2f1
FabioGafforelli <fabio.gafforelli@edu.univ-fcomte.fr>
2022-10-27 09:36:58 +0200

    retrait v-bind montre

--- src/pages/montres/panier.vue
+++ src/pages/montres/panier.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
     import type { Montre } from "@/types" 
 import MontreCarree from '@/components/MontreCarree.vue';
+
 </script>
 
 <template>
+    
     <main clas="bg-red-700 grid grid-flow-row-dense grid-cols-[repeat(auto-fit,minmax(350px,1fr))] gap-5  lg:mx-10">
     <div class="grid grid-cols-2">
         <div class="">
-            <MontreCarree class=" w-64" v-bind="montre" id="profil" />
+            <MontreCarree class=" w-64" id="profil" />
         </div>
         <div>
             <h3 class="text-xl font-bold">
