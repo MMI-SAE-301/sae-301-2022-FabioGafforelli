292bc2bdb1942b6cff0ab4f2367958a06e3d6276
FabioGafforelli <fabio.gafforelli@edu.univ-fcomte.fr>
2022-10-27 13:06:44 +0200

    bouton rond facebook

--- src/components/LoginFacebook.vue
+++ src/components/LoginFacebook.vue
@@ -4,7 +4,7 @@ import LogoFacebook from './LogoFacebook.vue';
 </script>
 <template>
 <div class="flex justify-center  ">
-                <div class="bg-bleuroi-100 mt-auto p-16 border-neutral-50 border-4 ">
+                <div class="bg-bleuroi-100 mt-auto p-16 border-neutral-50 border-4 rounded-full text-blanc">
     <button @pointerdown="supabase.auth.signIn({provider: 'facebook'})">
         <LogoFacebook />
         <p>Se connecter avec Facebook</p>
