ff8f3509f8e3ef1bd57969b21d00c55ae12b3f51
FabioGafforelli <fabio.gafforelli@edu.univ-fcomte.fr>
2022-10-27 13:41:56 +0200

    css connecter google

--- src/components/LoginGoogle.vue
+++ src/components/LoginGoogle.vue
@@ -4,7 +4,7 @@ import LogoGoogle from './LogoGoogle.vue';
 </script>
 <template>
 <div class="flex justify-center  ">
-                <div class=" mt-auto p-16 border-neutral-50 border-4 ">
+    <div class="bg-rouge mt-auto p-16 border-neutral-50 border-4 rounded-full text-blanc">
     <button class="text-white" @pointerdown="supabase.auth.signIn({provider: 'google'})">
         <LogoGoogle />
         Se connecter avec Google
