8d8839a73ced0b09fa78bbaab285cf351de451ae
FabioGafforelli <fabio.gafforelli@edu.univ-fcomte.fr>
2022-10-27 09:25:10 +0200

    suppression double header

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -2,7 +2,6 @@
 import Login from '@/components/Login.vue';
 import LoginFacebook from '@/components/LoginFacebook.vue';
 import LoginGoogle from '@/components/LoginGoogle.vue';
-import header from '@/components/header.vue';
 import { supabase, user } from '@/supabase';
 
 </script>
