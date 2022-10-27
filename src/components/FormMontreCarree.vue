9dee866c25645f7187e6ace7c9f1895b628b5b44
FabioGafforelli <fabio.gafforelli@edu.univ-fcomte.fr>
2022-10-27 09:09:10 +0200

    Réparation formulaire

--- src/components/FormMontreCarree.vue
+++ src/components/FormMontreCarree.vue
@@ -13,7 +13,7 @@ import {
       RadioGroupOption,
     } from '@headlessui/vue'
 
-const commander = ref(false)
+const enabled = ref(false)
 const router = useRouter();
 const montre = ref({});
 const { data: listeMateriaux, error } = await supabase
@@ -37,6 +37,17 @@ if (props.id) {
     console.log("n'a pas pu charger la table Montre :", error);
   else montre.value = data[0];
 }
+const quartierObject: Ref<any> = ref({});
+if (props.id) {
+  // On charge les données de la vue quartiercommune
+  let { data, error } = await supabase
+    .from("montre")
+    .select("*")
+    .eq("id", props.id);
+  if (error || !data)
+    console.log("n'a pas pu charger le table Maison :", error);
+  else quartierObject.value = data[0];
+}
 // @ts-ignore
 async function upsertMontre (dataForm, node) {
   const { data, error } = await supabase.from("montre").upsert(dataForm);
@@ -44,6 +55,23 @@ async function upsertMontre (dataForm, node) {
   else {
     node.setErrors([]);
     router.push({ name: "montres", params: { id: data[0].id } });
+  }
+}
+async function supprimerMontre() {
+const { data, error } = await supabase
+  .from('montre')
+  .delete()
+  .match({ id: quartierObject.value.id })
+
+  if (error) {
+    console.error(
+      "Erreur à la suppression de ",
+      quartierObject.value.id,
+      "erreur :",
+      error
+    );
+  } else {
+    router.push("/montres");
   }
 }
 </script>
@@ -66,7 +94,7 @@ async function upsertMontre (dataForm, node) {
                 <h1 class="text-white">Matériaux du bracelet</h1>
                 <br>
                 <div class="bg-bleuroi rounded-2xl pl-3 pr-3 py-3 mb-3">
-                  <RadioGroup v-model="montre" >
+                  <RadioGroup v-model="montre.id_materiaux" >
                     <RadioGroupLabel class="sr-only ">Server size</RadioGroupLabel>
                         <div class="flex gap-10 ">
                             <RadioGroupOption class=""
@@ -115,7 +143,17 @@ async function upsertMontre (dataForm, node) {
         <div>
                 <h1 class="text-white">Commander</h1>
                 <br>
-       
+        <Switch
+    v-model="montre.commander"
+    :class="montre.commander ? 'bg-bleuroi' : 'bg-bleuroi'"
+    class="relative inline-flex h-6 w-11 items-center rounded-full"
+  >
+    <span class="sr-only">Commander</span>
+    <span
+      :class="montre.commander ? 'translate-x-6' : 'translate-x-1'"
+      class="inline-block h-4 w-4 transform rounded-full bg-blanc transition"
+    />
+  </Switch>
         </div>
         <div>
                 <h1 class="text-white">Prix</h1>
@@ -124,6 +162,31 @@ async function upsertMontre (dataForm, node) {
                 <h2 class="text-blanc">249€</h2> <h2 class="line-through text-gris">Au lieu de 299€</h2>
             </div>
         </div>
+        <button
+        type="button"
+        v-if="quartierObject.id"
+        @click="($refs.dialogSupprimer as any).showModal()"
+        class="focus-style justify-self-end rounded-md bg-rouge p-2 shadow-sm"
+      >
+        Supprimer l'offre
+      </button>
+      <dialog
+        ref="dialogSupprimer"
+        @click="($event.currentTarget as any).close()"
+      >
+        <button
+          type="button"
+          class="focus-style justify-self-end rounded-md bg-blanc p-2 shadow-sm"
+        >
+          Annuler</button
+        ><button
+          type="button"
+          @click="supprimerMontre()"
+          class="focus-style rounded-md bg-rouge p-2 shadow-sm"
+        >
+          Confirmer suppression
+        </button>
+      </dialog>
         </FormKit>
             <br>
       </div>
